0b3352f28ef123a9e31da152c1eba6c5f1ed90eb
Nick <nprice0502@yahoo.com>
2023-05-04 18:01:50 -0400

    updated comment sections

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
   var currentHour = dayjs.hour();
   var saveNote = $(".saveBtn")
 
-  // make a loop for each time block div
+  // make a loop for each time block updating with the current time and changing the color of the time blocks
   var timeBlock = $(".description");
   timeBlock.each(function () {
     var timeBlockId = $(this).attr("id");
@@ -33,7 +33,7 @@ $(document).ready(function () {
     localStorage.setItem(id, task)
     showTask()
   })
-  
+
   // calling function when page first loads to get all items in local storage
   showTask()
 });
@@ -43,22 +43,5 @@ function showTask() {
     var foundTask = localStorage.getItem(i)
     $("#" + i).text(foundTask)
   }
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+ 
 };
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Daily-Scheduler
 
 ## Description
-* 
+* Here I updated the starter code given and added more time block divs. Using day.js I formated the day with the date.
 
 ## Github Repo URL
 * 
